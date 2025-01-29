89505ed61387735aea5ac075e86b068735f91b1b
Alvarogarcia91 <34041832+Alvarogarcia91@users.noreply.github.com>
2025-01-29 00:20:25 -0600

    app added to settings

--- comapa/settings.py
+++ comapa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*ta6*gfl5(w+=)@hvlw6!-4x6tvi*2!-44b#f!z#xtjzn&vc$s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cuentas',
 ]
 
 MIDDLEWARE = [
@@ -107,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
